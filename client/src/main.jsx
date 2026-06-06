84caf4a6c9d3ccc1c8ab3049ac9e0086280d92e5
ren <167855099+uamachan@users.noreply.github.com>
2026-06-06 22:37:20 +0900

    style: load forced matching icons css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,6 +11,7 @@ import './styles.css';
 import './profile-setup.css';
 import './profile-validation.css';
 import './design-polish.css';
+import './matching-icons.css';
 
 let firebaseModsPromise = null;
 function getFirebaseMods() {
